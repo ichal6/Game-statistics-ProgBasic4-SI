567e11c7edba1ce9b6fcc2b5bf77b66114771f27
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-08-14 11:22:14 +0200

    Add function open_file and implementation cont_games and decide
    In reports.py add function open_file. Fuction try open file with
    statistics. If function end with success then return list_games. If
    not print information about failure and run function
    shutdown_program.

--- reports.py
+++ reports.py
@@ -1,2 +1,36 @@
-
+import sys
 # Report functions
+
+
+def shutdown_program():
+    sys.exit()
+
+
+def open_file(file_name):
+    list_games = []
+    try:
+        with open(file_name, "r") as fileopen:
+            for line in fileopen:
+                line = line.replace('\n', '')  # replace new line with an empty char
+                list_games.append(line.split("\t"))  # divides the line of text according to tab
+            return list_games
+    except OSError:  # informs user if  can't find the file
+        print("File '" + file_name + "' not found!\nYour program has been close.")
+        shutdown_program()
+
+
+def count_games(file_name):
+    list_games = open_file(file_name)
+    count_of_games = len(list_games)
+    return count_of_games
+
+
+def decide(file_name, year):
+    list_games = open_file(file_name)
+    for game in list_games:
+        if int(game[2]) == year:
+            return True
+    return False
+
+
+print(decide("game_stat.txt", 1979))
