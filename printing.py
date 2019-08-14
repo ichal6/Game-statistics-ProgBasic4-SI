259a1cb0108662e27600525378d554012a980976
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-08-14 14:26:25 +0200

    Add function run_function and repair user_input
    Write function run_function. If reports doesn't exist yet, program
    print message about not working part.
    Reapir function user_input, beacause function didn't return  value of
    user number, but return True.

--- printing.py
+++ printing.py
@@ -1,4 +1,5 @@
 import os
+import reports
 from menu import Welcome_Message
 from menu import Menu_Chossen
 from menu import Correct_Input
@@ -8,7 +9,15 @@ from menu import Correct_Input
 NUMBER_OF_MENU_BEGIN = 1
 NUMBER_OF_MENU_END = 8
 
+# temporary value:
+
+file_with_statistics = "game_stat.txt"  # implement choose diffrent file name
+
 # Printing functions:
+
+
+def show_menu():
+    print(Menu_Chossen)
 
 
 def main():
@@ -17,6 +26,7 @@ def main():
     user_choice = False
     while user_choice is False:
         user_choice = user_input()
+    run_function(user_choice)
 
 
 def user_input():
@@ -24,7 +34,7 @@ def user_input():
         show_menu()
         user_choice = int(input())
         if user_choice in range(NUMBER_OF_MENU_BEGIN, NUMBER_OF_MENU_END + 1):
-            return True
+            return user_choice
         print(Correct_Input)
         return False
     except ValueError:
@@ -32,8 +42,23 @@ def user_input():
         return False
 
 
-def show_menu():
-    print(Menu_Chossen)
+def run_function(number_of_function):
+    if number_of_function == 1:
+        print("Count games in statistics -", str(reports.count_games(file_with_statistics)))
+    elif number_of_function == 2:
+        pass
+    elif number_of_function == 3:
+        pass
+    elif number_of_function == 4:
+        pass
+    elif number_of_function == 5:
+        pass
+    elif number_of_function == 6:
+        print("Not implemention, yet")
+    elif number_of_function == 7:
+        print("Not implemention, yet")
+    elif number_of_function == 8:
+        print("Not implemention, yet")
 
 
-main()
+main()  # run program
